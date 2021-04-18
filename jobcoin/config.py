a7ee224ac31932fe5b53389bfb06bbbae6582d66
supercerealized <connor@shields.mx>
2021-04-17 21:14:43 -0400

    Restructuring so tests are with application code pytests default import mode going forward isn't certain so avoiding relative imports with pytest seems more extensible pytest docs on this:https://docs.pytest.org/en/stable/goodpractices.html#tests-outside-application-code. Pytest dev branch discussion around this: https://github.com/pytest-dev/pytest/issues/7245 AND https://github.com/pytest-dev/pytest/pull/7870

---
+++ jobcoin/config.py
@@ -0,0 +1,4 @@
+# Replace the URL below
+API_BASE_URL = 'http://jobcoin.gemini.com/undaunted-gossip/api'
+API_ADDRESS_URL = '{}/addresses'.format(API_BASE_URL)
+API_TRANSACTIONS_URL = '{}/transactions'.format(API_BASE_URL)
