ef2059b5bc42bab616025faa3544283f18afdead
supercerealized <connor@shields.mx>
2021-04-19 10:42:08 -0400

    poll_and_process_deposit functionality validated

--- jobcoin/jobcoin.py
+++ jobcoin/jobcoin.py
@@ -5,54 +5,54 @@ from time import sleep
 
 from . import jobcoin_config
 
+def get_balance_and_transactions(address):
+	# Base endpoint utility
+
+	# TO DO: CHECK VARIABLE TYPE
+	# isinstance(address, str)
+
+	"""
+	address endpoint will never return 404 for an unused address, 
+	it will return a balance of 0 and an empty list of transactions. 
+	Numbers are returned as strings 
+	No 'fromAddress' indicates the Jobcoins were created on the Jobcoin website 
+	Returns type class dict
+	""" 
+	response = requests.get(jobcoin_config.API_ADDRESS_URL+'/{}'.format(address))
+	balance_and_transactions = json.loads(response.text)
+	return balance_and_transactions
+
+def get_all_transactions_list():
+	# Base endpoint utility
+
+
+	"""
+	Numbers are returned as strings 
+	No 'fromAddress' indicates the Jobcoins were created on the Jobcoin website 
+	Returns type class list
+	"""
+	response = requests.get(jobcoin_config.API_TRANSACTIONS_URL)
+	all_transactions_list = json.loads(response.text)
+	return all_transactions_list
+
+def send_jobcoins(fromAddress, toAddress, amount):
+	# Base endpoint utility
+
+	# TO DO: CHECK VARIABLE TYPE
+	# isinstance(fromAddress, str)
+	# isinstance(toAddress, str)
+	# isinstance(amount, str)
+
+	"""
+	<Response [200]> 'status': 'OK'
+	<Response [422]> 'error': 'Insufficient Funds'
+	"""
+	data = {'fromAddress':fromAddress, 'toAddress':toAddress, 'amount':amount}
+	
+	response = requests.post(jobcoin_config.API_TRANSACTIONS_URL, data = data)
+	return response.status_code 
+
 class JobcoinClient(object):
-
-	def get_balance_and_transactions(self, address):
-		# Base endpoint utility
-
-		# TO DO: CHECK VARIABLE TYPE
-		# isinstance(address, str)
-
-		"""
-		address endpoint will never return 404 for an unused address, 
-		it will return a balance of 0 and an empty list of transactions. 
-		Numbers are returned as strings 
-		No 'fromAddress' indicates the Jobcoins were created on the Jobcoin website 
-		Returns type class dict
-		""" 
-		response = requests.get(jobcoin_config.API_ADDRESS_URL+'/{}'.format(address))
-		balance_and_transactions = json.loads(response.text)
-		return balance_and_transactions
-
-	def get_all_transactions_list(self):
-		# Base endpoint utility
-
-
-		"""
-		Numbers are returned as strings 
-		No 'fromAddress' indicates the Jobcoins were created on the Jobcoin website 
-		Returns type class list
-		"""
-		response = requests.get(jobcoin_config.API_TRANSACTIONS_URL)
-		all_transactions_list = json.loads(response.text)
-		return all_transactions_list
-
-	def send_jobcoins(self, fromAddress, toAddress, amount):
-		# Base endpoint utility
-
-		# TO DO: CHECK VARIABLE TYPE
-		# isinstance(fromAddress, str)
-		# isinstance(toAddress, str)
-		# isinstance(amount, str)
-
-		"""
-		<Response [200]> 'status': 'OK'
-		<Response [422]> 'error': 'Insufficient Funds'
-		"""
-		data = {'fromAddress':fromAddress, 'toAddress':toAddress, 'amount':amount}
-		
-		response = requests.post(jobcoin_config.API_TRANSACTIONS_URL, data = data)
-		return response.status_code 
 
 	def poll_and_process_deposit(self, 
 				depositAddress = None,
@@ -63,14 +63,14 @@ class JobcoinClient(object):
 
 		depositAddress_data = get_balance_and_transactions(depositAddress)
 		depositAddress_balance = depositAddress_data['balance']
-		if depositAddress_balance not '0' and keeping_open is False:
-			# makes more sense to check if theres a transaction history to verify if its is (been) in use
+		if depositAddress_data['transactions'] != [] and keeping_open is False:
 			return 'account_in_use'
 		if validate_address is True:
 			return 'account_empty'
 		else:
 			initial_depositAddress_balance = depositAddress_balance
 			while initial_depositAddress_balance == depositAddress_balance:
+				print('checking balance')
 				depositAddress_balance = get_balance_and_transactions(depositAddress)['balance']
 				sleep(10)
 			if float(depositAddress_balance) - float(initial_depositAddress_balance) == float(expectedAmount):
